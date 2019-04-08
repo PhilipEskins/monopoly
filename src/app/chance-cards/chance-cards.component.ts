cc777068cc536bef17d318549448b61748ca3977
Maly Phongsavanh <phongsavanh619@icloud.com>
2019-04-08 11:38:57 -0700

    import ChanceCard & ChanceCardService to chance-cards.component

--- src/app/chance-cards/chance-cards.component.ts
+++ src/app/chance-cards/chance-cards.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ChanceCard } from '../chance-cards.model'
+import { ChanceCardsService } from '../chance-cards.service';
 
 @Component({
   selector: 'app-chance-cards',
   templateUrl: './chance-cards.component.html',
-  styleUrls: ['./chance-cards.component.scss']
+  styleUrls: ['./chance-cards.component.scss'],
+  providers: [ChanceCardsService]
 })
 export class ChanceCardsComponent implements OnInit {
 
