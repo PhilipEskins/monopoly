dac0e13e05110076f0b8c6657172eefb5b1cd7f7
Student <example@example.com>
2019-04-09 11:53:35 -0700

    allow car to move around the board

--- src/app/player.model.ts
+++ src/app/player.model.ts
@@ -1,6 +1,6 @@
 export class Player {
   public money: number = 1500;
-  public location: number = 0;
+  public location: number = 39;
   public propertiesOwned: number[];
     constructor (public name: string, public ifActive: boolean, public playerPiece: string) { }
   }
