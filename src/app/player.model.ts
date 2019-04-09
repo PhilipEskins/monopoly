ddcb9c993932c9c30bb92829f98a258b2d5c1115
Maly Phongsavanh and Nayomi Morita <maly+nayomi@>
2019-04-09 14:23:46 -0700

    enable player to buy property, activate owned null for property to true

--- src/app/player.model.ts
+++ src/app/player.model.ts
@@ -1,8 +1,8 @@
 export class Player {
   public money: number = 1500;
-  public location: number = 39;
-  public propertiesOwned: number[];
+  public location: number = 0;
     constructor (
+      public propertiesOwned: number[],
       public name: string,
       public ifActive: boolean,
       public playerPiece: string
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -21,7 +21,7 @@ export class PlayerComponent implements OnInit {
 
   constructor(private diceService: DiceService, private propertyService: PropertyService) { }
   players: Player =
-  new Player("Mr.Monopoly", true, "");
+  new Player([], "Mr.Monopoly", true, "");
 
 
 
@@ -33,6 +33,11 @@ export class PlayerComponent implements OnInit {
     this.roll1 = this.diceService.diceRoll();
     this.roll2 = this.diceService.diceRoll();
     this.doubleCheck();
+
+  }
+
+  consolelogging() {
+    console.log("hey" + this.players.propertiesOwned, this.prop[this.players.location].owner);
   }
 
   doubleCheck() {
@@ -78,7 +83,7 @@ export class PlayerComponent implements OnInit {
 
   buyingProperty() {
     console.log(this.players.location);
-    if(this.players.location===2 || this.players.location===7 || this.players.location===10 || this.players.location===17 || this.players.location===22 || this.players.location===33 || this.players.location===36){
+    if(this.players.location===2 || this.players.location===4 || this.players.location===7 || this.players.location===10 || this.players.location===17 || this.players.location===22 || this.players.location===33 || this.players.location===36 || this.players.location===38){
       alert("cant buy fool")
     } else {
       if (this.prop[this.players.location].owner!==null){
@@ -86,8 +91,12 @@ export class PlayerComponent implements OnInit {
       } else if (this.players.money<this.prop[this.players.location].price){
         alert("not enough funds");
       } else if (this.prop[this.players.location].owner == null && this.players.money>=this.prop[this.players.location].price){
-        alert("you can buy this prop");
+        if (confirm("Are you sure you want to buy this?")){
+          (this.players.propertiesOwned).push(this.prop[this.players.location].location);
+          return this.prop[this.players.location].owner = true;
+        }
       }
     }
   }
+
 }
